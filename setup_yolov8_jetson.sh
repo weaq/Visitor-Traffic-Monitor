761c12fc2d40bbb4b592833a9e9a19f63d1fe00b
weaq <30413309+weaq@users.noreply.github.com>
2025-07-30 10:24:37 +0700

    Update setup_yolov8_jetson.sh

--- setup_yolov8_jetson.sh
+++ setup_yolov8_jetson.sh
@@ -1,1 +1,8 @@
-pip install torch==1.10.0+nv21.09 torchvision==0.11.1+nv21.09 -f https://developer.download.nvidia.com/compute/redist/jp/v46
+# ไปที่โฟลเดอร์สำหรับดาวน์โหลด
+mkdir torch_install && cd torch_install
+
+# ดาวน์โหลด PyTorch 1.10.0 ที่รองรับ JetPack 4.6.1 (Python 3.6)
+wget https://nvidia.box.com/shared/static/p57jwntvok9467e3cfvk5i2rfsk4g2n2.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+
+# ดาวน์โหลด torchvision ที่เข้ากันได้
+wget https://nvidia.box.com/shared/static/f0zs3s7xqfr2eyu6qk8prb5hfz6vgyg1.whl -O torchvision-0.11.1-cp36-cp36m-linux_aarch64.whl
